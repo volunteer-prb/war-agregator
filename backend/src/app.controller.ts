d2a0600baf4e6c834fafc189ed032111d26a5e68
Albina <binanabiene@gmail.com>
2022-05-24 19:29:25 +0200

    Ran tools again against updated files

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, DefaultValuePipe, Get, NotFoundException, ParseIntPipe, Query } from '@nestjs/common';
-import { ApiExtraModels, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiResponse } from '@nestjs/swagger';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+import { ApiNotFoundResponse, ApiOkResponse, ApiQuery } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { ErrorDto } from './error.dto';
 import { ImageDto } from './image.dto';
 
-const dayInMS = 1000 * 60 * 60 * 24
+const dayInMS = 1000 * 60 * 60 * 24;
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
@@ -19,7 +25,8 @@ export class AppController {
   @ApiQuery({
     name: 'from',
     required: false,
-    description: 'Time until which images should be fetched. Number of ms since the 1st of Jan 1970. Default is now.',
+    description:
+      'Time until which images should be fetched. Number of ms since the 1st of Jan 1970. Default is now.',
   })
   @ApiQuery({
     name: 'limit',
@@ -27,34 +34,44 @@ export class AppController {
     description: 'Max number of images that should be fetched. Default 32.',
   })
   @ApiOkResponse({
-    type: [ImageDto]
+    type: [ImageDto],
   })
   @ApiNotFoundResponse({
     type: ErrorDto,
-    description: 'Not images found'
+    description: 'Not images found',
   })
   getGallery(
     @Query('from', new DefaultValuePipe(Date.now()), ParseIntPipe) from: number,
     @Query('limit', new DefaultValuePipe(32), ParseIntPipe) limit: number,
   ): ImageDto[] {
-    const thisDay = Array(limit / 2).fill(null).map((_, i) => {
-      return {
-        source: `http://fake-source-${i}-limit-${limit}-from-${new Date(from).toDateString().replace(' ', '_')}.com`,
-        originalImgUrl: 'https://www.fillmurray.com/3000/2000',
-        thumbnailImgUrl: 'https://www.fillmurray.com/300/200',
-        galleryImgUrl: 'https://www.fillmurray.com/1920/1080',
-        timestamp: new Date(from),
-      }
-    })
-    const prevDay = Array(limit / 2).fill(null).map((_, i) => {
-      return {
-        source: `http://fake-source-${i}-limit-${limit}-from-${new Date(from - dayInMS).toDateString().replace(' ', '_')}.com`,
-        originalImgUrl: 'https://www.fillmurray.com/3000/2000',
-        thumbnailImgUrl: 'https://www.fillmurray.com/300/200',
-        galleryImgUrl: 'https://www.fillmurray.com/1920/1080',
-        timestamp: new Date(from - dayInMS),
-      }
-    })
-    return [...thisDay, ...prevDay]
+    const thisDay = Array(limit / 2)
+      .fill(null)
+      .map((_, i) => {
+        return {
+          source: `http://fake-source-${i}-limit-${limit}-from-${new Date(from)
+            .toDateString()
+            .replace(' ', '_')}.com`,
+          originalImgUrl: 'https://www.fillmurray.com/3000/2000',
+          thumbnailImgUrl: 'https://www.fillmurray.com/300/200',
+          galleryImgUrl: 'https://www.fillmurray.com/1920/1080',
+          timestamp: new Date(from),
+        };
+      });
+    const prevDay = Array(limit / 2)
+      .fill(null)
+      .map((_, i) => {
+        return {
+          source: `http://fake-source-${i}-limit-${limit}-from-${new Date(
+            from - dayInMS,
+          )
+            .toDateString()
+            .replace(' ', '_')}.com`,
+          originalImgUrl: 'https://www.fillmurray.com/3000/2000',
+          thumbnailImgUrl: 'https://www.fillmurray.com/300/200',
+          galleryImgUrl: 'https://www.fillmurray.com/1920/1080',
+          timestamp: new Date(from - dayInMS),
+        };
+      });
+    return [...thisDay, ...prevDay];
   }
 }
