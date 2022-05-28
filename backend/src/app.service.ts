4aa4b0957bf4ce09cb93e4b26804652b7ac455e0
Alexey Tukalo <AIRTucha@users.noreply.github.com>
2022-05-28 06:40:24 +0200

    Initial structure of application for backend

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,7 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { DbService } from './db/db.service';
+import { FileStorageService } from './file-storage/file-storage.service';
+import { PacerService } from './pacer/pacer.service';
+import { RobustLoggerService } from './robust-logger/robust-logger.service';
+import { TelegramService } from './telegram/telegram.service';
+import { TwitterService } from './twitter/twitter.service';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnApplicationBootstrap {
+  constructor(
+    private readonly pacer: PacerService,
+    private readonly logger: RobustLoggerService,
+    private readonly telegram: TelegramService,
+    private readonly twitter: TwitterService,
+    private readonly db: DbService,
+    private readonly fileStorage: FileStorageService,
+  ) { }
+
+  private async collectData(startDate: Date) {
+    return Promise.all([
+      this.telegram.collectPictures(startDate),
+      this.twitter.collectPictures(startDate),
+    ]).then(data => data.flat());
+  }
+
+  async onApplicationBootstrap() {
+    this.pacer.run(async () => {
+      this.logger.info('Run code');
+      const lastTimeOfDataCollection = this.db.getLastCollectedTime();
+      const crawledPictureList = await this.collectData(
+        lastTimeOfDataCollection,
+      );
+      const pictureList = await this.fileStorage.saveToDist(crawledPictureList);
+      await this.db.save(pictureList);
+    });
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
---
+++ backend/src/telegram/telegram.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { DataIngestionServices } from '../DataIngestionServices';
+import { CrawledPicture } from '../pictures.entity';
+import { RobustLoggerService } from 'src/robust-logger';
+
+@Injectable()
+export class TelegramService implements DataIngestionServices {
+  constructor(private readonly logger: RobustLoggerService) {}
+
+  async collectPictures(startDate: Date): Promise<CrawledPicture[]> {
+    this.logger.info('Telegram data collection should be here', { startDate });
+    return [];
+  }
+}
